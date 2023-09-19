bd7ab4b1d6991c4ff51201431f5899b99444f4ef
Ahmed289m <2021170025@cis.asu.edu.eg>
2023-09-19 22:19:13 +0300

    add hive

--- lib/components/Custom_Text_Fields.dart
+++ lib/components/Custom_Text_Fields.dart
@@ -5,28 +5,28 @@ class CustomTextFileds extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(children: [
-       TextField(
-              decoration: InputDecoration(
-                  hintText: 'Title',
-                  hintStyle:
-                      TextStyle(color: Color.fromARGB(255, 119, 212, 202)),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white))),
-            ),
-             SizedBox(
-              height: 15,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 80, horizontal: 10),
-                  hintText: 'Content',
-                  hintStyle:
-                      TextStyle(color: Color.fromARGB(255, 119, 212, 202)),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white))),
-            ),
-    ],);
+    return const Column(
+      children: [
+        TextField(
+          decoration: InputDecoration(
+              hintText: 'Title',
+              hintStyle: TextStyle(color: Color.fromARGB(255, 119, 212, 202)),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white))),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        TextField(
+          decoration: InputDecoration(
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 80, horizontal: 10),
+              hintText: 'Content',
+              hintStyle: TextStyle(color: Color.fromARGB(255, 119, 212, 202)),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white))),
+        ),
+      ],
+    );
   }
 }
